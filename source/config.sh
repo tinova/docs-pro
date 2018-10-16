a24a53ffd353148a532e9d5a6412d380c9832a8a
Sergio Vega <svega@opennebula.systems>
2018-10-15 21:25:14 -0500

    Remove one-5.6-devel

--- source/config.sh
+++ source/config.sh
@@ -1,4 +1,3 @@
 one-5.6 5.6 173.255.245.62 /var/local/static/docs.opennebula.pro/
-one-5.6-maintenance 5.6-dev 173.255.245.62 /var/local/static/docs.opennebula.pro/
 one-5.4 5.4 173.255.245.62 /var/local/static/docs.opennebula.pro/
 master master 173.255.245.62 /var/local/static/docs.opennebula.pro/
