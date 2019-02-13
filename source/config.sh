ed7211836236773ab12af267e3567491e27a591b
Sergio Vega <svega@opennebula.systems>
2019-02-13 13:28:25 -0600

    Fix master branch

--- source/config.sh
+++ source/config.sh
@@ -1,3 +1,3 @@
 one-5.6 5.6 173.255.245.62 /var/local/static/docs.opennebula.pro/
 one-5.6-maintenance 5.6-dev 173.255.245.62 /var/local/static/docs.opennebula.pro/
-devel master 173.255.245.62 /var/local/static/docs.opennebula.pro/
+master devel 173.255.245.62 /var/local/static/docs.opennebula.pro/
