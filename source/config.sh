68eb38a993750062d5d08b32da813a8d68ee91c4
Sergio Vega <svega@opennebula.systems>
2019-02-13 13:27:49 -0600

    Change master to devel path

--- source/config.sh
+++ source/config.sh
@@ -1,6 +1,5 @@
-one-5.4 5.4 173.255.245.62 /var/local/static/docs.opennebula.pro/
 one-5.6 5.6 173.255.245.62 /var/local/static/docs.opennebula.pro/
 one-5.6-maintenance 5.6-dev 173.255.245.62 /var/local/static/docs.opennebula.pro/
 one-5.8 5.8 173.255.245.62 /var/local/static/docs.opennebula.pro/
 one-5.8-maintenance 5.8-dev 173.255.245.62 /var/local/static/docs.opennebula.pro/
-master master 173.255.245.62 /var/local/static/docs.opennebula.pro/
+master devel 173.255.245.62 /var/local/static/docs.opennebula.pro/
