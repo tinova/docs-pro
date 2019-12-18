cf3528f41cf97e884a15b0e066ac5ff9ff171e5e
Vlastimil Holer <vholer@opennebula.systems>
2019-12-18 13:49:53 +0100

    M #-: Fix GH links, add versions bar

---
+++ source/ext/versions.py
@@ -0,0 +1,18 @@
+def setup(app):
+    app.add_config_value('versions', [], 'html')
+    app.add_config_value('downloads', [], 'html')
+    app.add_config_value('github_version', '', 'html')
+
+    app.connect('html-page-context', create_versions)
+
+
+def create_versions(app, docname, templatename, ctx, doctree):
+    ctx['versions']  = app.builder.env.config['versions']
+    ctx['downloads'] = app.builder.env.config['downloads']
+
+    ctx['display_github'] = True
+    ctx['github_user']    = 'OpenNebula'
+    ctx['github_repo']    = 'docs-pro'
+    ctx['github_branch']  = 'onescape'
+    #ctx['conf_py_path']   = 'master/source/'
+    ctx['conf_py_path']   = 'onescape/source/'
